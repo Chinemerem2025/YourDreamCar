878d425e6de633abb76f52c4688d2e01db61b78d
Chinemerem <aganaphilipc@gmail.com>
2025-05-16 13:31:38 -0700

    Added dealership get/set

---
+++ YourDreamCar/src/main/java/com/yourdreamcar/Main.java
@@ -0,0 +1,23 @@
+package com.yourdreamcar;
+
+import com.yourdreamcar.ui.UserInterface;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserInterface.displayMenu();
+
+
+
+
+       // File file = new File("src/main/java/com/yourdreamcar/data/inventory.csv");
+      //  if (file.exists()) {
+      //      System.out.println("CSV file found: " + file.getAbsolutePath());
+     //   } else {
+     //       System.out.println("CSV file not found!");
+    //    }
+
+    }
+}
