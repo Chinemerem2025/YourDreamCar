b18be137da5ebb5670ba0f4860a8015a599bb548
Jesse Hernandez <101133566+jdez23@users.noreply.github.com>
2025-05-16 14:38:43 -0700

    added DealershipFileManager and a constructor in Vehicle

--- YourDreamCar/src/main/java/com/yourdreamcar/model/Vehicle.java
+++ YourDreamCar/src/main/java/com/yourdreamcar/model/Vehicle.java
@@ -8,7 +8,8 @@ public class Vehicle {
     private String vehicleType;
     private String color;
     private int odometer;
-    private  double price;
+    private double price;
+
 
     public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
         this.vin = vin;
@@ -19,21 +20,82 @@ public class Vehicle {
         this.color = color;
         this.odometer = odometer;
         this.price = price;
+    }
+
+    public Vehicle() {
+        this.vin = 0;
+        this.year = 0;
+        this.make = "";
+        this.model = "";
+        this.vehicleType = "";
+        this.color = "";
+        this.odometer = 0;
+        this.price = 0;
 
     }
 
-    public int getVin() { return vin; }
 
-    public int getYear() { return year; }
+    public int getVin() {
+        return vin;
+    }
 
-    public String getMake() { return make; }
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
 
-    public String getModel() { return model; }
-    public String getVehicleType() { return vehicleType; }
+    public int getYear() {
+        return year;
+    }
 
-    public String getColor() { return color; }
+    public void setYear(int year) {
+        this.year = year;
+    }
 
-    public int getOdometer() { return odometer; }
+    public String getMake() {
+        return make;
+    }
 
-    public double getPrice() { return price; }
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
